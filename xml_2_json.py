c297eab7619c008745e4a7fd97f892b3dd7331d0
perrone <rperrones@yahoo.com>
2019-08-27 16:36:50 -0300

    applying changes.

--- xml_2_json.py
+++ xml_2_json.py
@@ -7,6 +7,7 @@ Created on Mon Aug 26 19:32:22 2019
 
 """
 import xmltodict, json
+import xml.etree.ElementTree as ET
 import os
 import argparse
 import re
@@ -16,21 +17,36 @@ class XML_2_JSON:
     def __init__(self, path):
         dirs, filename = os.path.split(path)
         name, extension = filename.split(".")
-
+        self.json_file = name + '.json'
         if filename.endswith(".xml"):
-            with open(path) as fd:
-                self.doc = xmltodict.parse(fd.read())
-            
-            json_data = json.dumps(self.doc, sort_keys=True, indent=2)
-            json_data_processed = re.sub('."@', '"', json_data) 
-
-            output_file = open(name + '.json', 'w')
-            output_file.write(json_data_processed)
-
+            self.loadAsXML(path)
         else:
-            print('file extesion is not supported!')
+            self.loadAsJson(path, self.json_file)
+    
+    def getJson(self):
+        with open(self.json_file, 'r') as f:
+            collection = json.load(f)
+        return collection
+    
+    def loadAsJson(self, path, json_file):
+        with open(path) as fd:
+            doc = xmltodict.parse(fd.read())
+        
+        json_data = json.dumps(doc, sort_keys=True, indent=2)
+        json_data_processed = re.sub('."@', '"', json_data) 
 
-            
+        output_file = open(json_file, 'w')
+        output_file.write(json_data_processed)
+        
+    
+    def loadAsXML(self, path):
+        dirs, filename = os.path.split(path)
+        name, extension = filename.split(".")
+        if filename.endswith(".xml"):
+            tree = ET.parse(path)
+            self.root = tree.getroot()
+        
+    
 if __name__ == '__main__':
 
     # initiate the parser
@@ -38,3 +54,7 @@ if __name__ == '__main__':
     parser.add_argument("--annotation", help="path to xml file", default='./dataset/annotations/MVI_20011_v3.xml')
     args = parser.parse_args()
     jsonObj = XML_2_JSON(args.annotation)
+    a = jsonObj.getJson()
+    with open('./dataset/annotations/MVI_20011_v3.xml') as fd:
+         doc = xmltodict.parse(fd.read())
+    
