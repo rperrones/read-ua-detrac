ad3b7b5976919d97e9db220ae55123319edf35bc
perrone <rperrones@yahoo.com>
2019-08-27 10:11:19 -0300

    final version.

--- xml_2_json.py
+++ xml_2_json.py
@@ -8,25 +8,33 @@ Created on Mon Aug 26 19:32:22 2019
 """
 import xmltodict, json
 import os
-import pandas as pd
+import argparse
 import re
 
 class XML_2_JSON:
     
-    def __init__(self, filename):
+    def __init__(self, path):
+        dirs, filename = os.path.split(path)
+        name, extension = filename.split(".")
+
         if filename.endswith(".xml"):
-            with open(filename) as fd:
+            with open(path) as fd:
                 self.doc = xmltodict.parse(fd.read())
             
-            json_data = json.dumps(self.doc, sort_keys=True, indent=4)
-            print(json_data)
-            output_file = open('teste.json', 'w')
-            output_file.write(json_data)
+            json_data = json.dumps(self.doc, sort_keys=True, indent=2)
+            json_data_processed = re.sub('."@', '"', json_data) 
+
+            output_file = open(name + '.json', 'w')
+            output_file.write(json_data_processed)
 
         else:
             print('file extesion is not supported!')
 
             
 if __name__ == '__main__':
-    teste = XML2DataFrame('./dataset/annotations/MVI_20011_v3.xml')
-    
+
+    # initiate the parser
+    parser = argparse.ArgumentParser(prog='XML_2_JSON', usage='%(prog)s [options]', description='Convert a xml file to a json file.')
+    parser.add_argument("--annotation", help="path to xml file", default='./dataset/annotations/MVI_20011_v3.xml')
+    args = parser.parse_args()
+    jsonObj = XML_2_JSON(args.annotation)
